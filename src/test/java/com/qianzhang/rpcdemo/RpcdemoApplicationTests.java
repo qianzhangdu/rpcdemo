4cb7b2d0339d403a582bad52646126ce2822c272
qianzhang <qianzhang@qianzhangdeMacBook-Pro.local>
2021-09-02 11:23:54 +0800

    first commit

---
+++ src/test/java/com/qianzhang/rpcdemo/RpcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qianzhang.rpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
